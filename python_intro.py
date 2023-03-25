1a2fe1e3091fe4db486a6168767c567466293b79
adritamd <adritamd@gmail.com>
2023-03-25 16:01:42 -0500

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,51 @@
+name = 'adritamd'
+if name == 'adritamd':
+    print('Hey adritamd!')
+elif name == 'ale':  #elif añade otro if para lógica y funciones
+    print('Hey adritamd!')
+else:
+    print('Hey anonymous!')
+#comparar y lógica para rangos mayor/menor/igual y funciones
+#volume=57 
+#if volume<20:
+ #   print("It's kinda quite.")
+#elif 20 <= volume <40:
+ #   print("It's nice for background music")
+#elif 40 <= volume <60:
+ #   print("Perfect, I can hear all the details")
+#elif 60 <= volume <80:
+ #   print("Nice for parties")
+#elif 80<= volume <100:
+ #   print("A bit loud!")
+#else: 
+ #   print("Me duelen las orejas!:(")
+
+#def hi(): #definir "hi"
+    #print("Hi there!")
+    #print("How are you?")
+#hi()
+def hi(name): #definir a quién saludar según el nombre
+    if name == "adritamd":
+        print("Hi adritamd")
+    elif name == "ale":
+        print("Hi adritamd")
+    else:
+        print("Hi anonymous")
+hi("ale")
+#def hi(name):
+#    print("hi "+name+"!")
+#hi("Adri")
+
+#girls=["María", "Josefina", "Carolina", "Mariana"]
+#for name in girls: for hará que repita la función con cada elemento de girls en este caso
+#def hi(name):
+ #  print("Hi "+name+"!")
+#girls=["María", "Josefina", "Carolina", "Mariana"]  
+#for name in girls:
+#    hi(name)
+#    print("Next girl")
+#for con número y rangos (excel como base)
+
+#for i in range(1, 7):
+#    print(i)
+
